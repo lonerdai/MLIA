da9ca7037e894c297c6cbc886de1ccc2b10362e3
lonerdai <852877357@qq.com>
2015-07-07 20:41:07 +0800

    logistic

--- Logostic/logRegres.py
+++ Logostic/logRegres.py
@@ -2,7 +2,7 @@
 def loadDataSet():
     dataMat = [];labelMat = []
     fr = open('testSet.txt')
-    for line in fr.readlines:
+    for line in fr.readlines():
         lineArr = line.strip().split()
         dataMat.append([1.0,float(lineArr[0]),float(lineArr[1])])
         labelMat.append(int(lineArr[2]))
