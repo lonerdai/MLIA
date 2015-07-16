f8c7398c056aea461c936d99db0bd65ec496b107
lonerdai <852877357@qq.com>
2015-07-16 14:12:29 +0800

    画出最佳拟合直线的函数

--- Logostic/logRegres.py
+++ Logostic/logRegres.py
@@ -1,4 +1,6 @@
-﻿#Logistic 回归梯度上升优化算法
+﻿from numpy import *
+
+#Logistic 回归梯度上升优化算法
 def loadDataSet():
     dataMat = [];labelMat = []
     fr = open('testSet.txt')
@@ -12,6 +14,7 @@ def sigmoid(inX):
     return 1.0/(1+exp(-inX))
 
 def gradAscent(dataMatIn , classLabels):
+    '''梯度上升优化算法'''
     dataMatrix = mat(dataMatIn)
     labelMat = mat(classLabels).transpose()
     m,n = shape(dataMatrix)
@@ -24,3 +27,26 @@ def gradAscent(dataMatIn , classLabels):
         weights = weights + alpha * dataMatrix.transpose() * error
     return weights
 
+#画出数据集和logistic回归最佳拟合直线的函数
+def plotBestFit(weights):
+    '''此例子虽然简单且数据集很小，但是需要进行大量的运算，仍需做改进'''
+    import matplotlib.pyplot as plt
+    dataMat,labelMat=loadDataSet()
+    dataArr = array(dataMat)
+    n = shape(dataArr)[0] 
+    xcord1 = []; ycord1 = []
+    xcord2 = []; ycord2 = []
+    for i in range(n):
+        if int(labelMat[i])== 1:
+            xcord1.append(dataArr[i,1]); ycord1.append(dataArr[i,2])
+        else:
+            xcord2.append(dataArr[i,1]); ycord2.append(dataArr[i,2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    x = arange(-3.0, 3.0, 0.1)
+    y = (-weights[0]-weights[1]*x)/weights[2]
+    ax.plot(x, y)
+    plt.xlabel('X1'); plt.ylabel('X2');
+    plt.show()
